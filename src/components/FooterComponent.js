c2127863a028679f7c56f0d7bd67295c144e277f
aguspranyoto <agusprnyt@gmail.com>
2023-07-13 14:40:36 +0700

    first commit

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const FooterComponent = () => {
+  return (
+    <div className="footer">
+      <p>
+        Learn From
+        <a href="https://www.youtube.com/watch?v=LoYbN6qoQHA">
+          {" "}
+          Ope Afolabi's{" "}
+        </a>
+        Youtube Channel
+      </p>
+    </div>
+  );
+};
